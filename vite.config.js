eaedbdf3bb53a9ab3e07eb3e4fece24ef5c37531
Manish Mishra <manishmishra499@gmail.com>
2026-07-18 13:15:03 +0530

    deployment changes done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'my-react-app'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
   plugins: [react()],
   server: {
     // enable host so the dev server is reachable on the network when needed
